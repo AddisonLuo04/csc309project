1550e203c2ce5823759f5659184bc17effaf3f40
Luo Addison <luoaddis@teach.cs.toronto.edu>
2025-04-06 12:13:56 -0400

    set up cors so frontend can access backend, moved logic for starting server to server.js. start the backend by performing node server.js

--- A3/backend/index.js
+++ A3/backend/index.js
@@ -1,29 +1,41 @@
 #!/usr/bin/env node
 'use strict';
 
-const port = (() => {
-    const args = process.argv;
+// MOVE SERVER LOGIC TO SERVER.JS
+// const port = (() => {
+//     const args = process.argv;
 
-    if (args.length !== 3) {
-        console.error("usage: node index.js port");
-        process.exit(1);
-    }
+//     if (args.length !== 3) {
+//         console.error("usage: node index.js port");
+//         process.exit(1);
+//     }
 
-    const num = parseInt(args[2], 10);
-    if (isNaN(num)) {
-        console.error("error: argument must be an integer.");
-        process.exit(1);
-    }
+//     const num = parseInt(args[2], 10);
+//     if (isNaN(num)) {
+//         console.error("error: argument must be an integer.");
+//         process.exit(1);
+//     }
 
-    return num;
-})();
+//     return num;
+// })();
 
 const express = require("express");
+const cors = require('cors');
 const app = express();
 
 app.use(express.json());
 
-// ADD YOUR WORK HERE
+// set up cors to allow frontend access to the backend
+const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+
+app.use(cors({
+    origin: FRONTEND_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+}));
+
+
 // mount routes:
 const userRoutes = require('./src/routes/userRoutes');
 app.use('/users', userRoutes);
@@ -40,11 +52,14 @@ app.use('/events', eventRoutes);
 const transactionRoutes = require('./src/routes/transactionRoutes');
 app.use('/transactions', transactionRoutes);
 
-const server = app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
+// MOVE SERVER LOGIC TO SERVER.JS
+// const server = app.listen(port, () => {
+//     console.log(`Server running on port ${port}`);
+// });
 
-server.on('error', (err) => {
-    console.error(`cannot start server: ${err.message}`);
-    process.exit(1);
-});
+// server.on('error', (err) => {
+//     console.error(`cannot start server: ${err.message}`);
+//     process.exit(1);
+// });
+
+module.exports = app;
