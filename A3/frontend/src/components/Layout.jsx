ee7291f8cad9e259a0f0d4e63018a6733ab9e3b8
Alicia <aliciaz3136@gmail.com>
2025-04-06 03:02:57 -0700

    added material ui

--- A3/frontend/src/components/Layout.jsx
+++ A3/frontend/src/components/Layout.jsx
@@ -1,5 +1,6 @@
 import "./Layout.css";
-import { Link, Outlet } from "react-router-dom";
+import { Link as RouterLink, Outlet } from "react-router-dom";
+import { Link, AppBar} from '@mui/material';
 // for authorization
 // import { useAuth } from "../contexts/....";
 
@@ -7,13 +8,15 @@ const Layout = () => {
     // const { user, logout } = useAuth();
     return <>
         <header>
-            <Link to="/">Home</Link>
-            {/* { user ? <>
-                <Link to="/profile" className="user">{user.username}</Link>
-                <a href="#" onClick={logout}>Logout</a>
-                </> :
-                <Link to="/login">Login</Link>
-            } */}
+            {/* <AppBar color="#4a4e69">  --> trying app bar out but style isnt great so far*/}
+                <Link component={RouterLink} to="/">Home</Link>
+                {/* { user ? <>
+                    <Link to="/profile" className="user">{user.username}</Link>
+                    <a href="#" onClick={logout}>Logout</a>
+                    </> :
+                    <Link to="/login">Login</Link>
+                } */}
+            {/* </AppBar> */}
         </header>
         <main>
             <Outlet />
