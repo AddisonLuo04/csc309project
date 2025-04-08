e41b720472aff1b1c0243b85a164b5f1ad811ca5
Alicia <aliciaz3136@gmail.com>
2025-04-07 20:12:09 -0700

    superuser dashboard wip

--- A3/frontend/src/pages/Home.jsx
+++ A3/frontend/src/pages/Home.jsx
@@ -2,7 +2,8 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./main.css";
 import { useAuth } from "../contexts/AuthContext";
-import { Button, Modal } from '@mui/material';
+import { Button, Modal, Card, CardContent, CardActions, Accordion, AccordionSummary, AccordionDetails, Typography } from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import CreatePurchaseModal from "../components/createPurchaseModal";
 import ProcessRedemptionModal from "../components/ProcessRedemptionModal";
 import { useDashboard } from "../contexts/DashboardContext";
@@ -29,22 +30,35 @@ function Home() {
             {/* Regular*/}
                 <div>
                 <h2>Dashboard</h2>
-                <p><strong>Points: </strong>{user.points}</p> <br></br>
-                <p><strong>Recent Transactions </strong></p>
-                {recentTransactions && recentTransactions.length > 0 ? (
-                recentTransactions.map((transaction) => (
-                    <div key={transaction.id} className="transaction">
-                        <br></br>
-                        <p><strong>Transaction {transaction.id}:</strong></p>
-                        <p><strong>Type:</strong> {transaction.type}</p>
-                        <p><strong>Amount:</strong> ${transaction.amount}</p>
-                    </div>
-                ))
-                ) : (
-                    <p>No transactions available.</p>
-                )}
+                <Card variant="outlined" sx={{my: "8px"}}>
+                    <CardContent>
+                        <strong>Points: </strong>{user.points}
+                    </CardContent>
+                </Card>
+                <Card variant="outlined" sx={{my: "8px"}}>
+                    <CardContent>
+                        <strong>Recent Transactions</strong>
+                        {recentTransactions && recentTransactions.length > 0 ? (
+                        recentTransactions.map((transaction) => (
+                        <div key={transaction.id} className="transaction">
+                        <Accordion sx={{mt: "4px"}}>
+                            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                            <p style={{fontSize: "16px"}}><strong>Transaction {transaction.id}</strong></p>
+                            </AccordionSummary>
+                            <AccordionDetails>
+                            <p style={{fontSize: "14px"}}><strong>Type:</strong> {transaction.type}</p>
+                            <p style={{fontSize: "14px"}}><strong>Amount:</strong> ${transaction.amount}</p>
+                            </AccordionDetails>
+                        </Accordion>
+                        </div>
+                    ))
+                    ) : (
+                        <p>No transactions available.</p>
+                    )}
+                    </CardContent>
+                </Card>
                 </div>
-                <button>Show All Transactions</button>
+                <Button variant="outlined">Show All Transactions</Button>
             </> 
             : user.role === "cashier" ? <>
             {/* Cashier*/}
@@ -65,9 +79,47 @@ function Home() {
             : <>
             {/* Manager/Superuser*/}
                 <h2>Dashboard</h2>
-                <p>Manage Events</p>
-                <p>Manage Promotions</p>
-                <p>Manage Users</p>
+                <div style={{display: "flex", gap: "8px"}}>
+                    <Card variant="outlined" sx={{position: "relative", width: "250px", height: "250px"}}>
+                        <CardContent>
+                            <Typography gutterBottom variant="h6" component="div">
+                                Events
+                            </Typography>
+                            <Typography variant="body2">
+                                Eventinfo
+                            </Typography>
+                        </CardContent>
+                        <CardActions sx={{position: "absolute", bottom: "0"}}>
+                            <Button size="small" variant="outlined">Manage Events</Button>
+                        </CardActions>
+                    </Card>
+                    <Card variant="outlined" sx={{position: "relative", width: "250px", height: "250px"}}>
+                        <CardContent>
+                            <Typography gutterBottom variant="h6" component="div">
+                                Promotions
+                            </Typography>
+                            <Typography variant="body2">
+                                Eventinfo here
+                            </Typography>
+                        </CardContent>
+                        <CardActions sx={{position: "absolute", bottom: "0"}}>
+                            <Button size="small" variant="outlined">Manage Promotions</Button>
+                        </CardActions>
+                    </Card>
+                    <Card variant="outlined" sx={{position: "relative", width: "250px", height: "250px"}}>
+                        <CardContent>
+                            <Typography gutterBottom variant="h6" component="div">
+                                Users
+                            </Typography>
+                            <Typography variant="body2">
+                                Eventinfo here
+                            </Typography>
+                        </CardContent>
+                        <CardActions sx={{position: "absolute", bottom: "0"}}>
+                            <Button size="small" variant="outlined">Manage Users</Button>
+                        </CardActions>
+                    </Card>
+                </div>
             </>
         : ( <> {/* Not logged in*/}
             <h1>Welcome to A3!</h1>
